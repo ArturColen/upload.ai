05514fc2c40ca594cf6636ffb9a4f3fbd4fe4b1a
Artur Bomtempo Colen <arturbcolen@gmail.com>
2023-09-17 00:22:17 -0300

    feat: addition of the page header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { Button } from './components/ui/button';
+import { Github } from 'lucide-react';
+import { Separator } from './components/ui/separator';
+
 export function App() {
   return (
-    <h1>Hello World!</h1>
+    <div className='min-h-screen flex flex-col'>
+      <header className='px-6 py-3 flex items-center justify-between border-b'>
+        <h1 className='text-xl font-bold'>upload.ai</h1>
+        <div className='flex items-center gap-3'>
+          <span className='text-sm text-muted-foreground'>Desenvolvido por Artur Bomtempo</span>
+          <Separator orientation='vertical' className='h-6' />
+          <Button variant='outline'>
+            <Github className='w-4 h-4 mr-2' />
+            GitHub
+          </Button>
+        </div>
+      </header>
+    </div>
   );
 }
