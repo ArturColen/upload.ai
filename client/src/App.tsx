0f98b44a335724a446f2aa7b02f91054e7832c14
Artur Bomtempo Colen <arturbcolen@gmail.com>
2023-09-19 20:16:57 -0300

    feat(client): configuration of the form selection field and button status

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,8 +6,13 @@ import { Label } from './components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './components/ui/select';
 import { Slider } from './components/ui/slider';
 import { VideoInputForm } from './components/video-input-form';
+import { PromptSelect } from './components/prompt-select';
 
 export function App() {
+  function handlePromptSelected(template: string) {
+    console.log(template);
+  }
+
   return (
     <div className='min-h-screen flex flex-col'>
       <header className='px-6 py-3 flex items-center justify-between border-b'>
@@ -43,15 +48,7 @@ export function App() {
           <form className='space-y-6'>
           <div className='space-y-2'>
               <Label>Prompt</Label>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder='Selecione um prompt' />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value='title'>Título do YouTube</SelectItem>
-                  <SelectItem value='description'>Descrição do YouTube</SelectItem>
-                </SelectContent>
-              </Select>
+              <PromptSelect onPromptSelected={handlePromptSelected} />
             </div>
             <div className='space-y-2'>
               <Label>Modelo</Label>
